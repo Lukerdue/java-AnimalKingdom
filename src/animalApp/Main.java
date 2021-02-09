7d4621d0dad1784c50375d56a31609722825bab0
Jamie <jamielynjenks@gmail.com>
2021-02-09 15:02:22 -0700

    instantiated all the animals omg so much typinnnggg

--- src/animalApp/Main.java
+++ src/animalApp/Main.java
@@ -14,6 +14,18 @@ public class Main
         Mammal armadillo = new Mammal(1758, "Armadillo");
         Mammal racoon = new Mammal(1758, "Racoon");
         Mammal bigfoot = new Mammal(2021, "Bigfoot/Yeti");
+
+        //birds
+        Bird pigeon = new Bird("Pigeon", 1837);
+        Bird peacock = new Bird("Peacock", 1821);
+        Bird toucan = new Bird("Toucan", 1758);
+        Bird parrot = new Bird("Parrot", 1824);
+        Bird swan = new Bird("Swan", 1758);
+
+        //Fish
+        Fish salmon = new Fish("Salmon", 1758);
+        Fish catfish = new Fish("Catfish", 1817);
+        Fish perch = new Fish("Perch", 1758);
     }
     public static void main(String[] args)
     {
